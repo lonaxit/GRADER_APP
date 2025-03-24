65714b2077b69cd5e45c77902701b6102f81ca75
lonaxit <lonaxit@outlook.com>
2025-03-24 03:49:07 +0100

    Update serializers.py
    commted out serialier method fields

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -98,40 +98,40 @@ class ResumptionSettingSerializer(serializers.ModelSerializer):
 
 # subject teacher serializer
 class SubjectTeacherSerializer(serializers.ModelSerializer):
-    teacher = serializers.StringRelatedField(read_only=True)
-    teacher_name = serializers.SerializerMethodField()
-    session_name = serializers.SerializerMethodField()
-    subject_name = serializers.SerializerMethodField()
-    class_name = serializers.SerializerMethodField()
+    # teacher = serializers.StringRelatedField(read_only=True)
+    # teacher_name = serializers.SerializerMethodField()
+    # session_name = serializers.SerializerMethodField()
+    # subject_name = serializers.SerializerMethodField()
+    # class_name = serializers.SerializerMethodField()
     teacherid = serializers.SerializerMethodField()
 
     class Meta:
         model = SubjectTeacher
         fields = "__all__"
     
-    def get_teacher_name(self,object):
+    # def get_teacher_name(self,object):
                
-        teacherObj = User.objects.get(pk=object.teacher.pk)
-        return teacherObj.sur_name + ' ' + teacherObj.first_name
+    #     teacherObj = User.objects.get(pk=object.teacher.pk)
+    #     return teacherObj.sur_name + ' ' + teacherObj.first_name
     def get_teacherid(self,object):
                
         teacherObj = User.objects.get(pk=object.teacher.pk)
         return teacherObj.pk
     
-    def get_session_name(self,object):
+    # def get_session_name(self,object):
                
-        session = Session.objects.get(pk=object.session.pk)
-        return session.name
+    #     session = Session.objects.get(pk=object.session.pk)
+    #     return session.name
     
-    def get_class_name(self,object):
+    # def get_class_name(self,object):
                
-        _class = SchoolClass.objects.get(pk=object.classroom.pk)
-        return _class.class_name
+    #     _class = SchoolClass.objects.get(pk=object.classroom.pk)
+    #     return _class.class_name
     
-    def get_subject_name(self,object):
+    # def get_subject_name(self,object):
                
-        _subject = Subject.objects.get(pk=object.subject.pk)
-        return _subject.name
+    #     _subject = Subject.objects.get(pk=object.subject.pk)
+    #     return _subject.name
 
 
 
